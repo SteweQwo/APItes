60d24cacc1d4f57ece76b9f087a235c2eb221363
errr4567 <ega108242424@gmail.com>
2021-09-18 17:55:15 +0300

    Server

---
+++ TestAPI/TestAPI/Controllers/BudgetContext.cs
@@ -0,0 +1,23 @@
+﻿using TestAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestAPI.Controllers
+{
+    public class BudgetContext : DbContext
+    {
+
+        public BudgetContext(DbContextOptions<BudgetContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Budget> Budgets { get; set; }
+    }
+
+}
---
+++ TestAPI/TestAPI/Models/Budget.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestAPI.Models
+{
+    public class Budget
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public string Label_precentage { get => (Percentage * 100).ToString() + "%"; }
+        public float Percentage { get; set; }
+        public string Color { get; set; }
+    }
+}
